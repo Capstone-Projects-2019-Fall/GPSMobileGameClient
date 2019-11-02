828053cb2cc56f26d0f589df42e845a69924b1ae
Ewing Eng <eeng275@gmail.com>
2019-11-02 15:35:26 -0400

    added buffs

--- Assets/Scripts/Client/GameEngine/Combat/AbstractPlayer.cs
+++ Assets/Scripts/Client/GameEngine/Combat/AbstractPlayer.cs
@@ -8,12 +8,31 @@ public abstract class AbstractPlayer : MonoBehaviour
     private float memory = 10f;
     private bool alive = true;
     private bool combat = false;
+    private BuffHandler buffHandler = new BuffHandler();
     
     public float Memory { get => memory; set => memory = value; }
     public float Health { get => health; set => health = value; }
     public bool IsAlive { get => alive; set => alive = value; }
     public bool InCombat { get => combat; set => combat = value; }
+    public List<Buff> GetBuffList { get => buffHandler.buffList; }
     
+    public AbstractPlayer()
+    {
+
+    }
+
+    public AbstractPlayer(float health, float memory)
+    {
+        Health = health;
+        Memory = memory;
+        IsAlive = true;
+    }
+
+    public virtual void executeAttack(AbstractPlayer entity)
+    {
+        entity.damageReceived(0);
+    }
+
     public virtual void damageReceived(float damage)
     {
         Health -= damage;
@@ -22,7 +41,17 @@ public abstract class AbstractPlayer : MonoBehaviour
             IsAlive = false;
         }
     }
-    
+
+    public virtual void buffReceived(Buff buff)
+    {
+        buffHandler.addBuff(buff);
+    }
+
+    public virtual void buffRemoved(Buff buff)
+    {
+        buffHandler.removeBuff(buff);
+    }
+
     public virtual void startCombat() => InCombat = true;
     public virtual void endCombat() => InCombat = false;
 }
---
+++ Assets/Scripts/Client/GameEngine/Combat/BuffHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffHandler : MonoBehaviour
+{
+    public List<Buff> buffList = new List<Buff>();
+
+    public void addBuff(Buff buff)
+    {
+        buffList.Add(buff);
+    }
+
+    public void removeBuff(Buff buff)
+    {
+        buffList.Remove(buff);
+    }
+
+    public void handleBuffs()
+    {
+        foreach(Buff buff in buffList)
+        {
+            if(buff.BuffName == "")
+            {
+                /// execute code
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
