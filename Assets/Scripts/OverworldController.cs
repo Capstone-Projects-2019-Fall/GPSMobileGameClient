41a3f96f20cfae2bdbc9f9df27785f67bc2867c2
Tommyhillpicker <jordanreef@gmail.com>
2019-11-03 15:18:27 -0500

    Delete some meta files in Mapbox SDK

--- Assets/Scripts/OverworldController.cs
+++ Assets/Scripts/OverworldController.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OverworldController : MonoBehaviour
+public class OverworldController : Singleton<OverworldController>
 {
+    // Awake is called when the script is being loaded
+    private void Awake()
+    {
+       
+    }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
