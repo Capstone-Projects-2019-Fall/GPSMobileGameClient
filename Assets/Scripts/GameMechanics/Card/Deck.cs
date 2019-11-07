b8b6cd68715559ee38d29c3ff82f5810f55a04c3
Ewing Eng <eeng275@gmail.com>
2019-11-07 16:13:23 -0500

    hand deck implementation

---
+++ Assets/Scripts/GameMechanics/Card/Deck.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    [SerializeField] private int max_size = 40;
+    private List<Card> cards;
+
+    public int Max_Size { get => max_size; set => max_size = value; }
+
+    public Deck()
+    {
+
+    }
+
+    public Deck(int max_size)
+    {
+        Max_Size = max_size;
+        //TODO: add card handling?
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cards = new List<Card>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Adds card to list
+    public void AddCard(Card card)
+    {
+        cards.Add(card);
+    }
+
+    // Returns bool if card was removed or doesn't exist
+    public bool RemoveCard(Card card)
+    {
+        return cards.Remove(card);
+    }
+
+    // Copies card list over
+    public void CopyDeck(List<Card> cardList)
+    {
+        cards = new List<Card>(cardList);
+    }
+}
---
+++ Assets/Scripts/GameMechanics/Card/DeckManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    private Deck cards;
+    private Deck nonexhaustedCards;
+    private Deck hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cards = new Deck
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
