4b2df2e6f1a6b5e711edd8036ad4e834068f4cf1
Tommyhillpicker <jordanreef@gmail.com>
2019-11-08 00:39:03 -0500

    Baseline for overworld feature branch. Add initialization procedures to OverworldController.cs

--- Assets/Scripts/Overworld/OverworldController.cs
+++ Assets/Scripts/Overworld/OverworldController.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
+﻿using Mapbox.Unity.Map;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OverworldController : Singleton<OverworldController>
 {
-    // Awake is called when the script is being loaded
-    private void Awake()
-    {
-       
+    /// fields
+
+    private AbstractMap _map;
+    
+    /// accessors
+    public AbstractMap Map {
+        get => _map;
     }
 
-    // Start is called before the first frame update
+    //methods
+    
+    // Awake will be used to initialize static classes and unity singletons 
+    private void Awake()
+    {
+        try
+        {
+            _map = (AbstractMap) FindObjectOfType(typeof(AbstractMap));
+        } catch (Exception e) {
+            Debug.Log("Unable to find an instance of an AbstractMap");
+            // TODO: Configure and instantiate a new map
+        }
+
+        NodeFactory.InitializeFactory();
+        GpsUtility.InitialzeUtility(_map);
+
+    }
+
+    // Start will validate that all necessary scene objects/classes have been instantiated/initialized
     private void Start()
     {
         
