a038da031a221575e4a802fa9620b2634244010f
Ewing Eng <eeng275@gmail.com>
2019-12-01 18:34:26 -0500

    Fixed Camera Turning

--- Assets/Scripts/Overworld/BetterCamera.cs
+++ Assets/Scripts/Overworld/BetterCamera.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BetterCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject target;
+    public float damping = 1;
+    Vector3 offset;
+
     void Start()
     {
-        
+        offset = target.transform.position - transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
     }
 }
