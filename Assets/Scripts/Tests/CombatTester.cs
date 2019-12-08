da0dd285b5e7c2418de2c3d4656329b61e885767
Ewing Eng <eeng275@gmail.com>
2019-12-08 18:27:37 -0500

    More Combat Tests

--- Assets/Scripts/Tests/CombatTester.cs
+++ Assets/Scripts/Tests/CombatTester.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 
 public class CombatTester : MonoBehaviour
 {
-
-    [SerializeField] private Player _player;
-    [SerializeField] private Enemy _enemy;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public CombatController _cc;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Test Player receiving damage
+        _cc.Player.Health = 100;
+        _cc.ChangePlayerHealth(5);
+        Assert.AreEqual(95,_cc.Player.Health);
+
+        // Test Enemy receiving damage
+        _cc.Enemy.Health = 100;
+        _cc.ChangeEnemyHealth(5);
+        Assert.AreEqual(95, _cc.Enemy.Health);
+
+        // Test Player Drawcards
+        _cc.DrawCards(5);
+        Assert.AreEqual(5,_cc.Player.DeckManager.Hand.Cards.Count);
+
+        // Test Player losing memory.
+        _cc.Player.Memory = 5;
+        _cc.ChangeMemory(1);
+        Assert.AreEqual(4, _cc.Player.Memory);
     }
 }
