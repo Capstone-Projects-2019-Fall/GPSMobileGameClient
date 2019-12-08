a434e94ea1e62a2e6810c4add3f424bb4dfa57cc
Tommyhillpicker <jordanreef@gmail.com>
2019-12-08 18:44:49 -0500

    Extract CardInventoryZone prefab from the CraftingUI prefab, and add baseline for its corresponding controller script

---
+++ Assets/Scripts/UI/CardInventoryZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* CardInventoryZone Description:
+ * A CardInventoryZone is a flexible UI element used for displaying a graphical list of Cards. Similar to the CardScrollList, the main use cases
+ * for a CardInventoryZone are the crafting and deckbuilding/homebase scenes, although theoretically the prefab should be encapsulated properly and
+ * flexible enough to add to other scenes in the future. */
+public class CardInventoryZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
