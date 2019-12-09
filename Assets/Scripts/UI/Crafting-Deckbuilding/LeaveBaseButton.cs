68ec62947ef0daa4393da30a1acc233abc11effc
Tommyhillpicker <jordanreef@gmail.com>
2019-12-09 15:17:48 -0500

    Add LeaveBaseButton

---
+++ Assets/Scripts/UI/Crafting-Deckbuilding/LeaveBaseButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveBaseButton : MonoBehaviour
+{
+    public void LeaveBase()
+    {
+        SceneManager.LoadScene("GPSMobileGame");
+    }
+}
